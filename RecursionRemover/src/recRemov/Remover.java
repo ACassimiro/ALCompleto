10ac223f1e72eed546c869e3e125082500a58598
ACassimiro <cassimiroaellison@gmail.com>
2017-04-14 12:03:41 -0300

    Operational for n >= 0

--- RecursionRemover/src/recRemov/Remover.java
+++ RecursionRemover/src/recRemov/Remover.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 
 class Remover {
 
-	private static String getInput(String path) {
+	private static String getInput(int inputNumber) {
 		String content = "";
-		
+		String path = "src/files/input" + inputNumber + ".txt";
 		Scanner input = null;
         
         // Trying to create scanner
@@ -77,48 +77,68 @@ class Remover {
 		}
 	}
 	
-	static void printNTList (ArrayList<NonTerminal> ntList){
-		for (NonTerminal nonTerm : ntList){
-			System.out.println("Nome do NT: " + nonTerm.getName());
-			System.out.print("Lista de derivacoes: ");
-			
-			for (String exp : nonTerm.getGenerates()){
-				System.out.print( exp + " ");
+	static void printNTList (ArrayList<NonTerminal> ntList, int type, int outputNumber){
+		int auxSizeCount = 0;
+		
+		if(type == 0){
+			for (NonTerminal nonTerm : ntList){
+				System.out.println("Nome do NT: " + nonTerm.getName());
+				System.out.print("Lista de derivacoes: ");
+				
+				for (String exp : nonTerm.getGenerates()){
+					System.out.print( exp + " ");
+				}
+				
+				System.out.println("");
+			}			
+		} else if (type == 1){
+			try{
+			    PrintWriter writer = new PrintWriter("src/files/output" + outputNumber + ".txt", "UTF-8");
+			    
+			    for(NonTerminal nonTerm : ntList){
+			    	writer.print(nonTerm.getName() + " - ");
+			    	auxSizeCount = 0;
+			    	for(String generates : nonTerm.getGenerates()){
+			    		if(auxSizeCount++ ==  nonTerm.getGenerates().size() - 1){
+			    			writer.println(generates);
+			    		} else {
+			    			writer.print(generates + " | ");
+			    		}
+			    	}
+			    }
+			    
+			    writer.close();
+			} catch (IOException e) {
+			   // do something
 			}
-			
-			System.out.println("");
+		} else {
+			System.out.println("Unkown type");
 		}
 	}
 	
 	static ArrayList<NonTerminal> findPath (ArrayList<NonTerminal> ntList){
 		ArrayList<NonTerminal> path = new ArrayList<NonTerminal>();
-		
 		NonTerminal auxNT;
-				
+		char currentDeriv;
+		
 		auxNT = ntList.get(ntList.size() - 1); 
-		char currentDeriv = ntList.get(ntList.size() - 1).getName();
+		currentDeriv = ntList.get(ntList.size() - 1).getName();
+		
 		path.add(0, auxNT);		
 		while(true){
 			if(currentDeriv == ntList.get(0).getName())
 				break;			
 			
+			
 			for(NonTerminal nonTerm : ntList){
+				
 				if(nonTerm.getLeadsTo().contains(currentDeriv)){
 					auxNT = nonTerm;
 					currentDeriv = auxNT.getName();
 					path.add(0, auxNT);					
 					break;
 				}
-//				else {
-//					for(char gen: nonTerm.getLeadsTo()){
-//						System.out.print(gen + " ");
-//					}
-//					System.out.print("- does not generate " + currentDeriv + "\n");
-//				}
 			}
-			
-			
-
 		}
 		
 		return path;
@@ -141,6 +161,7 @@ class Remover {
 		if (ntList.size() > 1) {
 			
 			for(NonTerminal nonTerm : ntList){
+				
 				if(isFirst){
 					for(String generates : nonTerm.getGenerates()){
 						currentName = ntList.get(ntList.size() - 1).getName();
@@ -167,7 +188,6 @@ class Remover {
 				
 				if(auxSizeCounter == ntList.size())
 					break;
-				
 			}	
 			
 			currentName = ntList.get(ntList.size() - 1).getName();
@@ -181,7 +201,8 @@ class Remover {
 				
 		}
 		
-		dashedNT.generatesAdd(sufix + "X");
+		if(!sufix.isEmpty())
+			dashedNT.generatesAdd(sufix + "X");
 		
 		currentName = ntList.get(0).getName();
 		for(String generates : ntList.get(0).getGenerates()){
@@ -200,14 +221,17 @@ class Remover {
 		replace.leadsToAdd('X');
 		auxSizeCounter = 1;
 		prefix = "";
-		if (ntList.size() > 1) {
+
 			
+		if(ntList.size() != 1) {
+					
 			for(NonTerminal nonTerm : ntList){
 				auxNextPrefix = "";
 				currentName = ntList.get(auxSizeCounter).getName();
 				for(String generates : nonTerm.getGenerates()){
 					if(generates.indexOf(currentName) < 0){
-						replace.generatesAdd(generates + prefix + lastRecursionSufix + "X");
+						if(!replace.generatesContains(generates + prefix + lastRecursionSufix + "X"))
+							replace.generatesAdd(generates + prefix + lastRecursionSufix + "X");
 					} else {
 						auxNextPrefix = generates.substring(generates.indexOf(currentName) + 1, generates.length());
 					}			
@@ -227,23 +251,24 @@ class Remover {
 				
 				prefix = auxNextPrefix + prefix;
 			}	
-			
-			for(String generates : ntList.get(ntList.size() - 1).getGenerates()){
-				if(generates.indexOf(ntList.get(ntList.size() - 1).getName()) < 0 || generates.indexOf(ntList.get(0).getName()) < 0){
-					replace.generatesAdd(generates + "X");
-				}			
-			}
-			
-			for(char leadsTo : ntList.get(ntList.size() - 1).getLeadsTo()){
-				if(leadsTo == currentName || replace.leadsToContains(leadsTo) || replace.leadsToContains(ntList.get(0).getName()) || replace.leadsToContains(ntList.get(ntList.size() - 1).getName())){
-					continue;
-				} else {
-					replace.leadsToAdd(leadsTo);
-				}
-			}
-			
 		}
-		 
+		
+		for(String generates : ntList.get(ntList.size() - 1).getGenerates()){
+			if(generates.indexOf(ntList.get(ntList.size() - 1).getName()) < 0 || generates.indexOf(ntList.get(0).getName()) < 0){
+				if(!generates.isEmpty())
+					replace.generatesAdd(generates + "X");
+			}			
+		}
+		
+		for(char leadsTo : ntList.get(ntList.size() - 1).getLeadsTo()){
+			if(leadsTo == currentName || replace.leadsToContains(leadsTo) || replace.leadsToContains(ntList.get(0).getName()) || replace.leadsToContains(ntList.get(ntList.size() - 1).getName())){
+				continue;
+			} else {
+				replace.leadsToAdd(leadsTo);
+			}
+		}
+			
+	
 		ntList.remove(ntList.size() - 1);
 		
 		ntList.add(replace);
@@ -255,9 +280,12 @@ class Remover {
 	
 	public static void main (String []args){ 
 		String content;
+		int inOutNumber = 1;
+		int typeFileOrConsole = 1; // 0 for console, 1 for file
+		
 		ArrayList<NonTerminal> ntList = new ArrayList<NonTerminal>();
 		
-		content = getInput("src/files/input3.txt");
+		content = getInput(inOutNumber);
 
 		ntParser(content, ntList);
 
@@ -265,7 +293,7 @@ class Remover {
 
 		removeRecursion(ntList);
 		
-		printNTList(ntList);
+		printNTList(ntList, typeFileOrConsole, inOutNumber);
 	}
 }
 
